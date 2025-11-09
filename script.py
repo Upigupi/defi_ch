bf1ada772f4b51369bcbec460ee123c0bde5d292
Upigupi <upigypi33@gmail.com>
2025-11-09 13:25:33 +0300

    refactor: Remove obsolete dummy functions

--- script.py
+++ script.py
@@ -319,13 +319,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# @-internal-utility-start
-def get_config_value_8667(key: str):
-    """Reads a value from a simple key-value config. Added on 2025-11-09 13:24:35"""
-    with open('config.ini', 'r') as f:
-        for line in f:
-            if line.startswith(key):
-                return line.split('=')[1].strip()
-    return None
-# @-internal-utility-end
 
